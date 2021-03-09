2007e6d34973f795ef75aef3056eaeb5023dbf8c
Gomdi <ginpa0886@gmail.com>
2021-03-09 11:17:32 +0900

    로그인 확인 미들웨어

--- fontend/nail_house/src/Routes/CheckMiddleware/CheckMiddleware.js
+++ fontend/nail_house/src/Routes/CheckMiddleware/CheckMiddleware.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import styled from 'styled-components'
+import { userApi } from '../../api'
 
 const Padding = styled.section`
   margin-top:130px;
@@ -8,7 +9,34 @@ const Padding = styled.section`
 // 모든 페이지에 margin-top을 주기 위한 미들웨어 역할....
 // 왜냐면 header가 계속해서 위에 박혀있어야 하기 때문에...
 const CheckMiddleware = () => {
-  
+  // 토큰 유효성 검사하는 곳
+  const ChecktheLogined = async() => {
+    console.log("로그인 확인 실행됨");
+    try{
+      const token = localStorage.getItem("token")
+      const id = localStorage.getItem("user_id")
+      const res = await userApi.UserCheck(id, token);
+      // console.log(`res = ${res}`);
+      if(res){
+        // console.log("check");
+        return
+      }else{
+        localStorage.removeItem("user_id");
+        localStorage.removeItem("token");
+        localStorage.removeItem("profile");
+        return 
+      }
+    }catch(e){
+      localStorage.removeItem("user_id");
+      localStorage.removeItem("token");
+      localStorage.removeItem("profile");
+      // console.log("유저 정보가 만료되었습니다.");
+      return 
+    }
+  }
+
+  ChecktheLogined();
+
   return (
     <>
       <Padding></Padding>
