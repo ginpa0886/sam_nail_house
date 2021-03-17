9d0802cb37318f95f564b4fe06487a33cd41fc86
Gomdi <ginpa0886@gmail.com>
2021-03-17 22:25:48 +0900

    Cart DropBox

---
+++ fontend/nail_house/src/Routes/production/Children/ProductionBar/ProductionBar0.js
@@ -0,0 +1,284 @@
+import React, {useContext, useState} from 'react'
+import styled from 'styled-components'
+import '../../../../Asset/icomoonReal/style.css'
+import {ProductionContext} from '../../context'
+import '../../../../Asset/icomoon/style.css'
+import ProductionBar1 from './ProductionBar1'
+
+const Container = styled.section`
+  width:100%;
+  height:726px;
+  display:flex;
+  flex-direction:column;
+  justify-content:space-between;
+  align-items:center;
+`;
+
+
+const Footer = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+`;
+const TotalContainer = styled.div`
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+  margin-bottom:24px;
+`;
+const Word = styled.div`
+  font-size:13px;
+  line-height:1.538461538461538;
+  font-weight:700;
+  color:rgba(27, 28, 50, 1);
+`;
+const Price = styled.div`
+  font-size:20px;
+  line-height:1;
+  font-weight:700;
+  color:rgba(27, 28, 50, 1);
+`;
+
+const ButtonContainer = styled.div`
+  width:100%;
+  display:flex;
+  justify-content:center;
+  align-items:center;
+`;
+const ButtonOne = styled.button`
+  width:55px;
+  height:55px;
+  border: 1px solid rgba(224, 226, 231, 1);
+  border-radius:4px;
+  color:#E0E2E7;
+  background-color:white;
+  margin-right:8px;
+`;
+const Icon = styled.i`
+  font-size:32px;
+`;
+const ButtonTwo = styled.button`
+  width:100%;
+  height:55px;
+  border:1px solid #3DA8F5;
+  border-radius:4px;
+  background-color:white;
+  color:#3DA8F5;
+  font-size:18px;
+  letter-spacing:-0.02em;
+  font-weight:700;
+  line-height:1.555555555555556;
+  margin-right:5.23px;
+`;
+const ButtonSam = styled.button`
+  width:100%;
+  height:55px;
+  border:1px solid #3DA8F5;
+  border-radius:4px;
+  background-color:#3DA8F5;
+  color:white;
+  font-size:18px;
+  letter-spacing:-0.02em;
+  font-weight:700;
+  line-height:1.555555555555556;
+`;
+
+const LowContainer = styled.section`
+  height:100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:flex-start;
+  align-items:center;
+  padding-top:10px;
+  width:100%;
+`;
+const LowInCon = styled.div`
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  position:relative;
+  margin-bottom:8px;
+  background-color:#f5f5f5;
+  padding:10px;
+  width:100%;
+`;
+
+const LowCloseIcon = styled.i`
+  position:absolute;
+  top:8px;
+  right:5px;
+`;
+
+const LowHeadCon = styled.div`
+  width:100%;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:flex-start;
+`;
+const LowHead = styled.div`
+  font-size:15px;
+  line-height:20px;
+  color:#424242;
+  letter-spacing: -0.4px;
+  margin-bottom:10px;
+  padding-right:22px;
+`
+const LowSub = styled.div`
+  font-size:13px;
+  line-height:20px;
+  color:#424242;
+  margin-bottom:10px;
+  padding-right:22px;
+`;
+
+const LowFootCon = styled.div`
+  width:100%;
+  display:flex;
+  justify-content:space-between;
+  align-items:center;
+`;
+const LowDropboxCon = styled.div`
+  position:relative;
+  padding-left:10px;
+  background-color:white;
+  width:80px;
+  font-size:13px;
+  border-radius:3px;
+  line-height:22px;
+  height:24px;
+`;
+const LowDropboxContent = styled.div`
+  position: relative;
+`;
+const LowDropboxIcon = styled.i`
+  position:absolute;
+  top:6px;
+  right:10px;
+`;
+
+const LowTotal = styled.div`
+  font-size:15px;
+  font-weight:700;
+  line-height:24px;
+  color:#424242;
+  letter-spacing: -0.4px;
+`;
+
+const DropboxCon = styled.div`
+  width:100%;
+  display:${props => props.bgDropbox === true ? "flex" : "none"};
+  flex-direction:column;
+  justify-content:center;
+  align-items:flex-start;
+  position:absolute;
+  top: 20px;
+  left:0;
+  z-index:200;
+  background-color:white;
+`;
+const DropboxContent = styled.div`
+  width:100%;
+  
+  &:hover{
+    background-color:gray;
+    color:white;
+  }
+`;
+
+
+
+const ProductionBar0 = () => {
+  const { cart, cart: {production, price, count, totalmoney}, setCart } = useContext(ProductionContext)
+  const cartContainer = {
+    production,
+    price,
+    count
+  }
+  const [dropboxCheckArray, setdropboxCheckArray] = useState({
+    array:[]
+  })
+  const typeTotalmoney = totalmoney
+
+  const dropboxFns = (index) => {
+    const arrayNum = +index;
+    const typeArray = dropboxCheckArray.array;
+    let resultArray = [];
+    
+    if(typeArray.includes(arrayNum)){
+      resultArray = typeArray.filter(v => {
+        if(v === arrayNum){
+          return
+        }else{
+          return v
+        }
+      })
+    }else{
+      typeArray.push(arrayNum);
+      resultArray = typeArray
+    }
+    
+    setdropboxCheckArray({...dropboxCheckArray, array:resultArray})
+  }
+  console.log(dropboxCheckArray);
+  
+  return (
+    <>
+      <Container>
+        <ProductionBar1 />
+        
+        <LowContainer>
+        {/* product cart bar 반복문 */}
+        {cartContainer.production && cartContainer.production.map((value, index) => {
+          const bgCheckdropBox = dropboxCheckArray.array.includes(index) ? true : false;
+
+          return (
+            <LowInCon key={index}>
+              <LowCloseIcon className="icon-Close"></LowCloseIcon>
+              <LowHeadCon>
+                <LowHead>{cartContainer.production[index]}</LowHead>
+              </LowHeadCon>
+              <LowFootCon>
+                <LowDropboxCon>
+                  <LowDropboxContent onClick={() => dropboxFns(index)}>{cartContainer.count[index]}
+                    <DropboxCon bgDropbox={bgCheckdropBox}>
+                      <DropboxContent>1</DropboxContent>
+                      <DropboxContent>2</DropboxContent>
+                      <DropboxContent>3</DropboxContent>
+                      <DropboxContent>4</DropboxContent>
+                      <DropboxContent>5</DropboxContent>
+                      <DropboxContent>6</DropboxContent>
+                      <DropboxContent>7</DropboxContent>
+                      <DropboxContent>8</DropboxContent>
+                      <DropboxContent>9</DropboxContent>
+                    </DropboxCon>
+                  </LowDropboxContent>
+                  <LowDropboxIcon className="icon-Chevron"></LowDropboxIcon>
+                </LowDropboxCon>
+                <LowTotal>{(cartContainer.price[index] * count[index]).toLocaleString()}원</LowTotal>
+              </LowFootCon>
+            </LowInCon>
+          )
+          })}
+        </LowContainer>
+        <Footer>
+          <TotalContainer>
+            <Word>주문금액</Word>
+            <Price>{(+typeTotalmoney).toLocaleString()}원</Price>
+          </TotalContainer>
+          <ButtonContainer>
+            <ButtonOne><Icon className="icon-Bookmark"></Icon></ButtonOne>
+            <ButtonTwo>장바구니</ButtonTwo>
+            <ButtonSam>바로구매</ButtonSam>
+          </ButtonContainer>
+        </Footer>
+      </Container>
+    </>
+  )
+}
+
+export default ProductionBar0
--- fontend/nail_house/src/Routes/production/Children/ProductionBar/ProductionBar1.js
+++ fontend/nail_house/src/Routes/production/Children/ProductionBar/ProductionBar1.js
@@ -1,19 +1,12 @@
-import React from 'react'
+import React, {useContext, useState} from 'react'
 import styled from 'styled-components'
-import '../../../../Asset/icomoonReal/style.css'
+import {ProductionContext} from '../../context'
 
-const Container = styled.section`
-  width:100%;
-  height:726px;
-  display:flex;
-  flex-direction:column;
-  justify-content:space-between;
-  align-items:center;
-`;
 const InputArea = styled.div`
+  width:100%;
 `;
 
-const InputMain = styled.input`
+const InputMain = styled.div`
   width:100%;
   height:40px;
   border:1px solid #3DA8F5;
@@ -24,9 +17,10 @@ const InputMain = styled.input`
   color:rgba(63, 65, 80, 1);
   margin-bottom:8px;
   padding:8px 16px;
+  position:relative;
 `;
 
-const InputSub = styled.input`
+const InputSub = styled.div`
   width:100%;
   height:40px;
   border:1px solid rgba(224, 226, 231, 1);
@@ -36,100 +30,189 @@ const InputSub = styled.input`
   letter-spacing:-0.01em;
   color:rgba(63, 65, 80, 1);
   padding:8px 16px;
+  position: relative;
 `;
 
-const Footer = styled.div`
+const OptionBarContainer = styled.section`
   width:100%;
+  max-height:300px;
+  overflow-y:scroll;
+  position:absolute;
+  top:40px;
+  left:0;
+  display:${props => props.bgProps === false ? "none" : "flex"};
+  flex-direction:column;
+  justify-content:flex-start;
+  align-items:center;
+  margin-top:4px;
+  z-index:100;
+`;
+
+const OptionItem = styled.div`
+  width:100%;
+  background-color:white;
+  border:1px solid rgba(224, 226, 231, 1);;
+  border-radius:4px 4px 0 0;
+  padding:8px 20px 8px 20px;
   display:flex;
   flex-direction:column;
   justify-content:center;
-  align-items:center;
-`;
-const TotalContainer = styled.div`
-  width:100%;
-  display:flex;
-  justify-content:space-between;
-  align-items:center;
-  margin-bottom:24px;
-`;
-const Word = styled.div`
-  font-size:13px;
-  line-height:1.538461538461538;
-  font-weight:700;
-  color:rgba(27, 28, 50, 1);
-`;
-const Price = styled.div`
-  font-size:20px;
-  line-height:1;
-  font-weight:700;
-  color:rgba(27, 28, 50, 1);
+  align-items:flex-start;
+
+  &:hover{
+    cursor: pointer;
+    background-color:rgba(224, 226, 231, 1);
+  }
 `;
 
-const ButtonContainer = styled.div`
-  width:100%;
+
+
+const OptDiv = styled.div``;
+const OptDivTwo = styled.div`
   display:flex;
-  justify-content:center;
+  justify-content:flex-start;
   align-items:center;
 `;
-const ButtonOne = styled.button`
-  width:55px;
-  height:55px;
-  border: 1px solid rgba(224, 226, 231, 1);
-  border-radius:4px;
-  color:#E0E2E7;
-  background-color:white;
-  margin-right:8px;
+
+const OptioName = styled.div`
+  color:#424242;
+  font-size:13px;
+  line-height:1.230769230769231;
 `;
-const Icon = styled.i`
-  font-size:32px;
-`;
-const ButtonTwo = styled.button`
-  width:100%;
-  height:55px;
-  border:1px solid #3DA8F5;
-  border-radius:4px;
-  background-color:white;
-  color:#3DA8F5;
-  font-size:18px;
-  letter-spacing:-0.02em;
+
+const OptionSale = styled.div`
+  margin-right:6px;
+  color:#35c5f0;
+  font-size:17px;
+  line-height:1.352941176470588;
   font-weight:700;
-  line-height:1.555555555555556;
-  margin-right:5.23px;
 `;
-const ButtonSam = styled.button`
-  width:100%;
-  height:55px;
-  border:1px solid #3DA8F5;
+const OptionPrice = styled.div`
+  margin-right:5px;
+  color:#000;
+  font-size:17px;
+  line-height:1.352941176470588;
+  font-weight:700;
+`;
+
+const OptionSpecial = styled.div`
   border-radius:4px;
-  background-color:#3DA8F5;
+  padding:1px 3px;
+  background-color:rgb(255, 119, 119);
   color:white;
-  font-size:18px;
-  letter-spacing:-0.02em;
-  font-weight:700;
-  line-height:1.555555555555556;
+  font-size:11px;
 `;
 
 const ProductionBar1 = () => {
+  const { detail: { productioninfo }, cart, cart: {production, price, count, totalmoney, productionId, optionId, loading}, setCart } = useContext(ProductionContext)
+  const { option, production : { info : { original_price }} } = productioninfo;
+  const optionArray = option;
+  const options = productioninfo.option;
+  
+  const typeOption = options
+  
+  const [dropbox, setDropbox] = useState({
+    mainDisplay:false,
+    subDisplay:false
+  })
+
+  // dropbox display
+  const displayClick = (num) => {
+    const typeNum = num;
+    if(typeNum === 1){
+      if(dropbox.mainDisplay === true){
+        setDropbox({...dropbox, mainDisplay:false})
+      }else{
+        setDropbox({...dropbox, mainDisplay:true})
+      }
+    }else{
+      if(dropbox.subDisplay === true){
+        setDropbox({...dropbox, subDisplay:false})
+      }else{
+        setDropbox({...dropbox, subDisplay:true})
+      }
+    }
+  }
+
+  
+    const AddProduction = (e) => {
+      const { target : { children }} = e;
+      const name = children[0].innerText;
+      const cost = children[1].children[1].innerText; 
+  
+      // string형태의 자릿수에 (,)콤마가 찍혀있는 것들 제거해주는 replace함수
+      const typecost = cost.replace(",", "");
+      
+      // State에 total 금액 계산
+      // typecost가 string이므로 이를 Number로 바꿔주기의해 +typecost로 작성
+      const total = totalmoney + (+typecost);
+      
+      // option_id 와 production_id를 담기게 하는 것
+      const chckoptionId = [];
+       optionArray.map((value) => {
+        if(value.option_name === name){
+          chckoptionId.push(value.option_id, value.production_option_id)
+          return 
+        }
+      })
+    
+       if(!production.includes(name)){
+        setCart({...cart, production:[...production, name], price:[...price, typecost], count:[...count, 1], productionId:[...productionId, chckoptionId[1]], optionId:[...optionId, chckoptionId[0]], totalmoney:total, loading:true})
+       }else{
+         alert("이미 담겨진 상품입니다.")
+       }
+    }
+  
+
+
   return (
-    <>
-      <Container>
-        <InputArea>
-          <InputMain placeholder="선택" />
-          <InputSub placeholder="추가상품 (선택)" />
-        </InputArea>
-        <Footer>
-          <TotalContainer>
-            <Word>주문금액</Word>
-            <Price>0원</Price>
-          </TotalContainer>
-          <ButtonContainer>
-            <ButtonOne><Icon className="icon-Bookmark"></Icon></ButtonOne>
-            <ButtonTwo>장바구니</ButtonTwo>
-            <ButtonSam>바로구매</ButtonSam>
-          </ButtonContainer>
-        </Footer>
-      </Container>
-    </>
+    <InputArea>
+      <InputMain onClick={() => displayClick(1)}>선택
+        <OptionBarContainer bgProps={dropbox.mainDisplay}>
+          {typeOption && typeOption.map((value, index) => {
+            const sale = 100 - Math.floor((value.option_sell_price / original_price) * 100);
+            if(value.option_type === 0){
+              return (
+                <OptionItem key={index} onClick={AddProduction}>
+                  <OptDiv>
+                    <OptioName>{value.option_name}</OptioName>
+                  </OptDiv>
+                  <OptDivTwo>
+                    <OptionSale>{sale}%</OptionSale>
+                    <OptionPrice>{(+value.option_sell_price).toLocaleString()}</OptionPrice>
+                    <OptionSpecial>특가</OptionSpecial>
+                  </OptDivTwo>
+                </OptionItem>
+              )
+            }else{
+              return
+            }
+          })}
+        </OptionBarContainer>
+      </InputMain>
+      <InputSub onClick={() => displayClick(2)}>추가상품 (선택)
+        <OptionBarContainer bgProps={dropbox.subDisplay}>
+          {typeOption && typeOption.map((value, index) => {
+            
+            if(value.option_type === 1){
+              return (
+                <OptionItem key={index} onClick={AddProduction}>
+                  <OptDiv>
+                    <OptioName>{value.option_name}</OptioName>
+                  </OptDiv>
+                  <OptDivTwo>
+                    <OptionPrice>{(+value.option_sell_price).toLocaleString()}</OptionPrice>
+                    <OptionSpecial>특가</OptionSpecial>
+                  </OptDivTwo>
+                </OptionItem>
+              )
+            }else{
+              return
+            }
+          })}
+        </OptionBarContainer>
+      </InputSub>
+    </InputArea>
   )
 }
 
