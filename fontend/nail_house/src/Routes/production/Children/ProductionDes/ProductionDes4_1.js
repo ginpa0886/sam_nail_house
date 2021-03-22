94fdbe7d6badeeeb09c0c09fc1e394431fbcd022
Gomdi <ginpa0886@gmail.com>
2021-03-22 10:13:32 +0900

    dropbox

--- fontend/nail_house/src/Routes/production/Children/ProductionDes/ProductionDes4_1.js
+++ fontend/nail_house/src/Routes/production/Children/ProductionDes/ProductionDes4_1.js
@@ -31,6 +31,10 @@ const TotalProduct = styled.div`
 `;
 const TotalIcon = styled.div`
   color:#757575;
+
+  &:hover{
+    cursor:pointer;
+  }
 `;
 
 
@@ -53,7 +57,7 @@ const TotalCount = styled.div`
   padding:3px 5px;
   position:relative;
 `;
-const TotalButton = styled.button`
+const TotalButton = styled.div`
   background-color:white;
   border:none;
 
@@ -92,6 +96,28 @@ const CountButton = styled.button`
   }
 `;
 
+const DropboxCon = styled.div`
+  width:100%;
+  display:${props => props.bgDropbox === true ? "flex" : "none"};
+  flex-direction:column;
+  justify-content:center;
+  align-items:flex-start;
+  position:absolute;
+  top: 20px;
+  left:0;
+  z-index:200;
+  background-color:white;
+`;
+const DropboxContent = styled.div`
+  width:100%;
+  text-align:left;
+  
+  &:hover{
+    background-color:gray;
+    color:white;
+  }
+`;
+
 const ProductionDes4_1 = () => {
   const { cart, cart: {production, price, count}, setCart } = useContext(ProductionContext)
   const cartContainer = {
@@ -99,15 +125,69 @@ const ProductionDes4_1 = () => {
     price,
     count
   }
-  const dropbox = [1,2,3,4,5,6,7,8,9,10];
-  
 
-  const changeTotal = (e) => {
+  const [dropboxCheckArray, setdropboxCheckArray] = useState({
+    array:[]
+  })
+  
+  // dropbox active관리
+  const dropboxFns = (index) => {
+    const arrayNum = +index;
+    const typeArray = dropboxCheckArray.array;
+    let resultArray = [];
     
+    if(typeArray.includes(arrayNum)){
+      resultArray = typeArray.filter(v => {
+        if(v === arrayNum){
+          return
+        }else{
+          return v
+        }
+      })
+    }else{
+      typeArray.push(arrayNum);
+      resultArray = typeArray
+    }
+    
+    setdropboxCheckArray({...dropboxCheckArray, array:resultArray})
+  }
+
+  // dropbox에서 숫자 선택시 상품 선택 갯수 변와 및 총 가격 업데이트 하는 함수
+  const changeCount = (e) => {
+    
+    const {target:{innerText, id}} = e;
+    const typeText = +innerText;
+    const typeId = id;
+    const cartCountArray = cart.count;
+    cartCountArray[typeId] = typeText;
+
+    // 총 구입액 구하는 곳
+    const typeTotal = {
+      total:0
+    }
+    cartContainer.price.map((value, index) => typeTotal.total += value * cartContainer.count[index]);
+    
+
+    setCart({...cart, count:cartCountArray, totalmoney:typeTotal.total});
+  }
+
+  // 상품 삭제 함수
+  const removeProduction = (num) => {
+    const typeIndex = num;
+    cartContainer.production.splice(typeIndex, 1);
+    cartContainer.price.splice(typeIndex, 1);
+    cartContainer.count.splice(typeIndex, 1);
+
+    // 총 구입액 구하는 곳
+    const typeTotal = {
+      total:0
+    }
+    cartContainer.price.map((value, index) => typeTotal.total += value * cartContainer.count[index]);
+
+    setCart({...cart, production:cartContainer.production, price:cartContainer.price, count:cartContainer.count, totalmoney:typeTotal.total});
   }
   
-  // console.log(cartContainer);
-  // console.log(cartContainer.production.length);
+ 
   return (
     <>
       <TotalSection>
@@ -115,22 +195,30 @@ const ProductionDes4_1 = () => {
             <>
               {cartContainer.production.map((value, index) => {
                 const totalPrice = cartContainer.price[index] * cartContainer.count[index];
+                const bgCheckdropBox = dropboxCheckArray.array.includes(index) ? true : false;
                 return(
                   <ForKey key={index}>
                     <TotalContainer>
                       <TotalOne>
                         <TotalProduct>{value}</TotalProduct>
-                        <TotalIcon className="icon-Close"></TotalIcon>
+                        <TotalIcon className="icon-Close" onClick={() => removeProduction(index)}></TotalIcon>
                       </TotalOne>
                       <TotalTwo>
-                        <TotalCount onClick={changeTotal}>
-                          <TotalButton>{cartContainer.count[index]}</TotalButton>
+                        <TotalCount onClick={() => dropboxFns(index)}>
+                          <TotalButton>{cartContainer.count[index]}
+                            <DropboxCon bgDropbox={bgCheckdropBox}>
+                              <DropboxContent onClick={changeCount} id={index}>1</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>2</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>3</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>4</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>5</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>6</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>7</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>8</DropboxContent>
+                              <DropboxContent onClick={changeCount} id={index}>9</DropboxContent>
+                            </DropboxCon>
+                          </TotalButton>
                           <TotalButtonIcon className="icon-Chevron"></TotalButtonIcon>
-                          <CountDropboxContainer>
-                            {dropbox.map((value, index) => 
-                              <CountButton key={index}>{value}</CountButton>
-                            )}
-                          </CountDropboxContainer>
                         </TotalCount>
                         <TotalPrice>{(+totalPrice).toLocaleString()}원</TotalPrice>
                       </TotalTwo>
